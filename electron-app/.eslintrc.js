2b02b143aa4bd19f77062fa28e143737766948c8
AlexeySitnikov <alexey.sitnikov@bk.ru>
2024-05-08 12:56:28 +0300

    just install eslint to electron-app

--- electron-app/.eslintrc.js
+++ electron-app/.eslintrc.js
@@ -1,25 +1,36 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
-    },
-    "extends": "airbnb-base",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    rules: {
-        semi: ['error', 'never'],
-        'react/jsx-uses-react': 0,
-        'react/react-in-jsx-scope': 0,
-        'react/jsx-props-no-spreading': 0,
-        'react/prop-types': 0,
-        'no-console': 0,
-        'import/no-cycle': 0,
-        'import/prefer-default-export': 0,
-        'react/jsx-filename-extension': 0,
-        'linebreak-style': 0,
-      },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: ['error', 'never'],
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'no-console': 0,
+    'import/no-cycle': 0,
+    'import/prefer-default-export': 0,
+    'react/jsx-filename-extension': 0,
+    'linebreak-style': 0,
+    'import/no-extraneous-dependencies': 0,
+    'global-require': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'import/no-relative-packages': 0,
+  },
 }
